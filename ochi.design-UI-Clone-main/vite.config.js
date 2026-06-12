8a89662b0b572fc0c58cac4c5f1b4bdc9ca2e5b2
mathewluvchriste <mathewluvchriste@gmail.com>
2026-06-12 16:44:30 +0800

    Fix GitHub Pages base path

--- ochi.design-UI-Clone-main/vite.config.js
+++ ochi.design-UI-Clone-main/vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ochi.design-UI-Clone/',
+  base: '/We-are-Youth-and-Sports/',
 })
